ead904b1c1fa63a3e08e88531133305e6b1de1f0
Tarek Inoubli <tarekinoubli@gmail.com>
2022-01-14 13:35:33 +0100

    Add SignalR services

--- ChatWithSignalR/Chat.SignalR/Program.cs
+++ ChatWithSignalR/Chat.SignalR/Program.cs
@@ -1,6 +1,13 @@
+using Chat.SignalR.Hubs;
+
 var builder = WebApplication.CreateBuilder(args);
+
+// Add SignalR Middleware services
+builder.Services.AddSignalR();
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Add the Hub as an endpoint
+app.MapHub<ChatHub>("/chat");
 
 app.Run();
